8f1af17f27af31f35b7457203b63bf674333279f
FilipLangiewicz <flangiewicz@gmail.com>
2023-11-29 21:05:00 +0100

    Commit z brancha FilipLangiewicz dla testu

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,8 @@ public class Main {
 
         System.out.println("Kocham Jave");
 
-        System.out.println(Main.add(1,2));
-
     }
 
-    public int dodajLiczby(int a, int b) {
-        return a + b;
-
-    }
-
-    public void pobierzDaneZSerweraIPrzekazDoBase() {
-
-
-    }
-
-
-    public static int add(int a, int b) {
-        return a + b;
-    }
 
 
 }
